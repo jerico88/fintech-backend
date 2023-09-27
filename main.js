363e09ab28badca9f4f99b560c45d7706cbfac79
Jerico <Jerico-@bitbucket.org>
2023-09-27 21:43:47 +0800

    updated apis and database

--- main.js
+++ main.js
@@ -1,38 +1,14 @@
 const express = require("express");
-const mysql = require("mysql");
+const user = require("./apis/user");
+const accounts = require("./apis/accounts");
 
-// Create connection
-const db = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    password: "123",
-    port: 3306
-});
+const service = express();
 
-// Connect to MySQL
-db.connect((error) => {
-    if (error) throw error;
-    console.log("MySQL connected!")
-});
+service.use(express.json());
+service.use("/user", user.router);
+service.use("/accounts", accounts.router);
 
-// const user = require("./apis/user");
-const app = express();
-
-// Create DB
-app.get('/createdb', (req, res) => {
-    let sql = 'CREATE DATABASE employees';
-    db.query(sql, (error, result) => {
-        if (error) throw error;
-        console.log(result);
-        res.send('DB created!');
-    });
-});
-
-// app.use(express.json());
-// app.use("/user", user.router);
-// app.use("/account", account.router);
-
-app.listen(
+service.listen(
     3000,
     (error) => {
         if (error) console.error("Error occurred while starting app");
