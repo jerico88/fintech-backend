5173dccbe0b981ab7f05e736a8e89e8fa1cb4437
lijianxu1406 <lijianxu1406@gmail.com>
2023-09-28 18:28:23 +0800

    added Edit and delete function Postman

--- apis/testapi.js
+++ apis/testapi.js
@@ -11,7 +11,7 @@ const router = express.Router();
 //const salary = document.getElementById('salary');
 
 // get all records (Postman)
-router.get("/get-all", (request, response) => {
+router.get("/view-all", (request, response) => {
     database.mysqlConnection.query("SELECT * FROM EmployeeRecords", (error, results) => {
         if (error) {
             console.error(error);
@@ -34,6 +34,19 @@ router.get("/get-by-id", (request, response) => {
     });
 });
 
+//delete record by id (postman)
+router.delete("/delete-by-id", (request,response)=>{
+    database.mysqlConnection.query(`DELETE from EmployeeRecords where id=${request.query.id}`, (error,results) => {
+        if (error) {
+            console.log(error);
+            response.status(500).send("Internal Server Error");
+        } else {
+            console.log("Deleted");
+            response.status(200).send("Deleted");
+        }
+    })
+});
+
 //Add user function (Postman)
 router.post("/add", (request,response)=>{
     const { name, employee_id, job_title, salary } = request.body; //how to connect this part to frontend input
@@ -44,12 +57,25 @@ router.post("/add", (request,response)=>{
         } else {
             if (results["affectedRows"] != 0) {
                 console.log("Added");          
+                response.status(200).send("Added");
             }
         }
     })
 });
 
-
+//Edit user function (Postman)
+router.put("/edit-by-id", (request,response)=>{
+    const { name, employee_id, job_title, salary } = request.body; //how to connect this part to frontend input
+    database.mysqlConnection.query(`Update EmployeeRecords set name = '${name}', employee_id = '${employee_id}', job_title = '${job_title}', salary = '${salary}' where id = ${request.query.id}`, (error,results) => {
+        if (error) {
+            console.error(error);
+            response.status(500).send("Internal Server Error");
+        } else {
+            console.log("Modified");
+            response.status(200).send("Modified");
+        }
+    })
+});
 
 
 module.exports = { router };
